0e6be56562f1e4952e89d5a2663891b1d766f1bc
tech-White-Snow <tech.whitesnow@gmail.com>
2023-11-10 13:56:37 -0500

    add new mockups

--- client/src/utils/Constant.js
+++ client/src/utils/Constant.js
@@ -4,22 +4,34 @@ export const PUBLIC_URL = "http://localhost:3000";
 export const mockups = [
     {
         group: "Laptop",
-        mockups: ["Laptop (1)"] 
+        mockups: [
+            "Laptop (1)",
+            "Laptop (2)",
+            "Laptop (3)",
+            "Laptop (4)",
+        ] 
     },
     {
         group: "Book",
         mockups: [
-                "Book (1)",
-                "Book (2)",
-                "Book (3)",
-                "Book (4)",
-                "Book (5)",
-                "Book (6)",
-                "Book (7)",
-                "Book (8)",
-                "Book (9)",
-                "Book (10)",
-            ] 
+            "Book (1)",
+            "Book (2)",
+            "Book (3)",
+            "Book (4)",
+            "Book (5)",
+            "Book (6)",
+            "Book (7)",
+            "Book (8)",
+            "Book (9)",
+            "Book (10)",
+            "Book (11)",
+            "Book (12)",
+            "Book (13)",
+            "Book (14)",
+            "Book (15)",
+            "Book (16)",
+            "Book (17)",
+        ] 
     },
     {
         group: "Mug",
@@ -27,11 +39,48 @@ export const mockups = [
     },
     {
         group: "Ipad",
-        mockups: ["Ipad (1)"] 
+        mockups: [
+            "Ipad (1)",
+            "Ipad (2)",
+            "Ipad (3)",
+            "Ipad (4)",
+            "Ipad (5)",
+            "Ipad (6)",
+        ] 
     },
     {
         group: "Phone",
-        mockups: ["Phone (1)","Phone (2)","Phone (3)","Phone (4)", "Phone (5)"] 
+        mockups: [
+            "Phone (1)",
+            "Phone (2)",
+            "Phone (3)",
+            "Phone (4)", 
+            "Phone (5)"
+        ] 
     },
-    
+    {
+        group: "Box",
+        mockups: [
+            "Box (1)",
+            "Box (2)",
+            "Box (3)",
+            "Box (4)"
+        ] 
+    },
+    {
+        group: "Mac",
+        mockups: [
+            "Mac (1)",
+            "Mac (2)",
+            "Mac (3)"
+        ] 
+    },
+    {
+        group: "Media Player",
+        mockups: [
+            "Media Player (1)",
+            "Media Player (2)",
+            "Media Player (3)"
+        ] 
+    },
 ];
