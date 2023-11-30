482fe482e1e79e5d4e046fc3b8e2b5919e77530c
tech-White-Snow <tech.whitesnow@gmail.com>
2023-11-30 08:37:12 -0500

    fixed wrong server domain

--- client/src/utils/Constant.js
+++ client/src/utils/Constant.js
@@ -1,5 +1,5 @@
-export const backendUrl = "https://abundanceauthority.com/";
-export const PUBLIC_URL = "https://abundanceauthority.com/";
+export const backendUrl = "https://abundanceauthority.com";
+export const PUBLIC_URL = "https://abundanceauthority.com";
 
 export const mockups = [
     {
